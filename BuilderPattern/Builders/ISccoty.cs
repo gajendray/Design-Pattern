11d9befe2d521eb7d6af0e18c581bfee03cbe126
Prakash Gudipati <bgudipati.software@gmail.com>
2020-04-11 20:26:00 +0530

    Builder pattern

---
+++ BuilderPattern/Builders/ISccoty.cs
@@ -0,0 +1,14 @@
+﻿using BuilderPattern.Concrete;
+
+namespace BuilderPattern.Builders
+{
+    public interface IScooty
+    {
+        Scooty SetName(string name);
+        Scooty SetCC(int cc);
+        Scooty BreakType(string breakType);
+        Scooty fuelCapacity(int fuelCapacity);
+        Scooty maxSpeed(int speed);
+        string GetScooty();
+    }
+}
---
+++ BuilderPattern/Models/BikeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern.Models
+{
+    public class BikeModel
+    {
+        private string _breakType;
+        private int _fuelCapacity;
+        private int _maxSpeed;
+        private int _noOfGears;
+        private int _cc;
+        private string _name;
+
+        public string BreakType { get => _breakType; set => _breakType = value; }
+        public int FuelCapacity { get => _fuelCapacity; set => _fuelCapacity = value; }
+        public int MaxSpeed { get => _maxSpeed; set => _maxSpeed = value; }
+        public int NoOfGears { get => _noOfGears; set => _noOfGears = value; }
+        public int Cc { get => _cc; set => _cc = value; }
+        public string Name { get => _name; set => _name = value; }
+    }
+}
